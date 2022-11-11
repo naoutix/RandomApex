56b4b7e0e8503ca55ea93ac69a903bf0405aa17e
naoutix <jleo.dary@gmail.com>
2022-11-11 11:43:47 +0100

    fix: Fix wrong tirage

--- script/main.js
+++ script/main.js
@@ -1,5 +1,4 @@
 //Script Element
-const NombreLegends = 23
 let NameNumber = ['bloodhound',
                   'gibraltar',
                   'lifeline',
@@ -46,12 +45,32 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function counterLegends(legendselector){
+    count = 0
+    for (let i = 0; i < 5; i++) {
+        var max = 5
+        if (i == 4){
+            max = 3
+        }
+        for (let j = 0; j < max; j++) {
+            const myRow = legendselector.getElementsByTagName("tr")[i];
+            const myCell = myRow.getElementsByTagName("td")[j];
+            const checkbox = myCell.getElementsByTagName("input")
+            if (checkbox.checkbox.checked){
+                count++
+            }
+
+        }
+    }
+    return count
+}
+
 function reset(tab,legendselector){
     if (tab.length == 0) {
         let listPlayer = []
         for (let i = 0; i < 5; i++) {
             var max = 5
-            if (i ==4){
+            if (i == 4){
                 max = 3
             }
             for (let j = 0; j < max; j++) {
@@ -93,27 +112,32 @@ function reset(tab,legendselector){
 function changeimage(contexte,path) {
     contexte.src = path
 }
-function findTheOne(max1,max2,max3){
-    if (max1.length == 1 || max1.length == 3 || max1.length == 4){
-        return [max1,1]
-    }
-    if (max2.length == 1 || max2.length == 3 || max2.length == 4){
-        return [max2,2]
-    }
-    if (max2.length == 1 || max3.length == 3 || max3.length == 4){
-        return [max3,3]
-    }
-    return [null,-1]
+
+function sort(max1,max2,max3){
+    
+    //Index Sort
+    let l = [max1.length,max2.length,max3.length]
+    SortList = l.map(function(e,i){return {ind: i,val: e}})
+    SortList.sort(function(x,y){return x.val - y.val})
+    indice = SortList.map(function(e){return e.ind})
+    
+    //List sort
+    temp = [max1,max2,max3]
+    MaxSort = indice.map(function(e){return temp[e]})
+    return {indice,MaxSort}
 }
 
-
-function generateFirstElement(max1,max2,max3){
+function generateFirstElement(MaxSort){
+    let max1 = MaxSort[0]
     choose1 = max1[getRandomInt(max1.length)]
     max1.splice(max1.indexOf(choose1),1)
     return choose1
 }
 
-function generateSecondElement(max1,max2,max3,choose1) {
+function generateSecondElement(MaxSort,choose1) {
+    let max1 = MaxSort[0]
+    let max2 = MaxSort[1]
+    let max3 = MaxSort[2]
     var choose2 = choose1
     var k = 0
     while (choose2 == choose1 && k <3000){
@@ -121,47 +145,28 @@ function generateSecondElement(max1,max2,max3,choose1) {
         choose2 = elementChoisi
         if (max1.length == 1){
             if (max2.length == 2){
-                if (max3.length == 1){
-                    if (max3.indexOf(elementChoisi)!=-1){
-                        choose2 = choose1
-                    }
-                }
-                else{
-                    for (let index = 0; index < max2.length; index++) {
-                        const elementAutre = max2[index];
-                        if (elementAutre != elementChoisi){
-                            var change = false
-                            if (max1.indexOf(elementAutre)!=-1){
-                                change = true
-                                console.log('1')
-                            }
-                            if (max3.length == 2 && max3.indexOf(elementAutre)!=-1 && max3.indexOf(max1[0])!=-1){
-                                change = true
-                                console.log('3')
-                            }
-                            if (max3.length == 2 && max3.indexOf(elementChoisi)!=-1 && max3.indexOf(choose1)!=-1){
-                                change = true
-                                console.log('4')
-                            }
-                            if (max3.length == 2 && max3.indexOf(elementAutre)!=-1 && max3.indexOf(choose1)!=-1){
-                                change = false
-                                console.log('2')
-                            }
-                            if (max3.length == 2 && max3.indexOf(elementChoisi)!=-1 && max3.indexOf(max1[0])!=-1){
-                                change = false
-                                console.log('5')
-                            }
-                            if (change) {
-                                choose2 = elementAutre
-                            }
+                for (let index = 0; index < max2.length; index++) {
+                    const elementAutre = max2[index];
+                    if (elementAutre != elementChoisi){
+                        var change = false
+                        if (max1.indexOf(elementAutre)!=-1){
+                            change = true
                         }
-                    }
-                }
-            }
-            if (max2.length > 2){
-                if (max3.length == 2){
-                    if (max3.indexOf(elementChoisi)!=-1){
-                        choose2 = choose1
+                        if (max3.length == 2 && max3.indexOf(elementAutre)!=-1 && max3.indexOf(max1[0])!=-1){
+                            change = true
+                        }
+                        if (max3.length == 2 && max3.indexOf(elementChoisi)!=-1 && max3.indexOf(choose1)!=-1){
+                            change = true
+                        }
+                        if (max3.length == 2 && max3.indexOf(elementAutre)!=-1 && max3.indexOf(choose1)!=-1){
+                            change = false
+                        }
+                        if (max3.length == 2 && max3.indexOf(elementChoisi)!=-1 && max3.indexOf(max1[0])!=-1){
+                            change = false
+                        }
+                        if (change) {
+                            choose2 = elementAutre
+                        }
                     }
                 }
             }
@@ -175,7 +180,10 @@ function generateSecondElement(max1,max2,max3,choose1) {
     return choose2
 }
 
-function generateThirdElement(max1,max2,max3,choose1,choose2) {
+function generateThirdElement(MaxSort,choose1,choose2) {
+    let max1 = MaxSort[0]
+    let max2 = MaxSort[1]
+    let max3 = MaxSort[2]
     var choose3 = choose2
     var k = 0
     while ((choose1 == choose3 || choose2 == choose3)&& k <3000){
@@ -224,70 +232,105 @@ function generateThirdElement(max1,max2,max3,choose1,choose2) {
     return choose3  
 }
 
+//button.addEventListener('click',generate)
+button.addEventListener('click',generate2)
 
 
-button.addEventListener('click',generate)
-function generate() {
-    max1 = reset(max1,myTable1)
-    max2 = reset(max2,myTable2)
-    max3 = reset(max3,myTable3)
-    clearTimeout(timeout1)
-    clearTimeout(timeout2)
-    clearTimeout(timeout3)
-
-    var [priormax,player] = findTheOne(max1,max2,max3)
-    if (player != -1){
-        var indexprior = 0
-        for (let index = 0; index < priormax.length; index++) {
-            const element = priormax[index];
-    
-            if (max1.indexOf(element) != -1 && max2.indexOf(element) != -1 && max3.indexOf(element) != -1 ){
-                indexprior = index
+function uncheckLegends(num,legendselector) {
+    const nomLegends = NameNumber.indexOf(num)
+    for (let i = 0; i < 5; i++) {
+        var max = 5
+        if (i == 4){
+            max = 3
+        }
+        for (let j = 0; j < max; j++) {
+            const myRow = legendselector.getElementsByTagName("tr")[i];
+            const myCell = myRow.getElementsByTagName("td")[j];
+            const checkbox = myCell.getElementsByTagName("input")
+            if (nomLegends == checkbox.checkbox.value){
+                checkbox.checkbox.checked = false
+                return 1
             }
         }
-        if (player == 1){
-            choose1 = max1[indexprior]
-            max1.splice(max1.indexOf(choose1),1)
-            choose2 = generateSecondElement(max1,max2,max3,choose1)
-            choose3 = generateThirdElement(max1,max2,max3,choose1,choose2)
-        }
-        if (player == 2){
-            choose2 = max2[indexprior]
-            max2.splice(max2.indexOf(choose2),1)
-            choose1 = generateSecondElement(max2,max1,max3,choose2)
-            choose3 = generateThirdElement(max2,max1,max3,choose2,choose1)   
-        }
-        if (player == 3){
-            choose3 = max3[indexprior]
-            max3.splice(max3.indexOf(choose3),1)
-            choose2 = generateSecondElement(max3,max2,max1,choose3)
-            choose1 = generateThirdElement(max3,max2,max1,choose3,choose2)   
-        }      
     }
-    else
-    {
-        choose1 = generateFirstElement(max1,max2,max3)
-        choose2 = generateSecondElement(max1,max2,max3,choose1)
-        choose3 = generateThirdElement(max1,max2,max3,choose1,choose2)
-    }
-    /*
-    console.log(`#RUN#`)
-    console.log(`${choose1}:${choose2}:${choose3}:`)
-    console.log(`${max1}:${max2}:${max3}:`)
-    */
+    return -1
+}
+
+function generate2() {
     // Reset random 
     team1.src = `images/random1.gif`
     team2.src = `images/random2.gif`
     team3.src = `images/random3.gif`
 
-    timeout1 = setTimeout(changeimage.bind(null,team1,`images/${NameNumber[choose1]}.png`),1000)
-    timeout2 = setTimeout(changeimage.bind(null,team2,`images/${NameNumber[choose2]}.png`),3000)
-    timeout3 = setTimeout(changeimage.bind(null,team3,`images/${NameNumber[choose3]}.png`),6000)
+
+    max1 = reset(max1,myTable1)
+    max2 = reset(max2,myTable2)
+    max3 = reset(max3,myTable3)
+    // Check Solo
+    let Counters = [counterLegends(myTable1),counterLegends(myTable2),counterLegends(myTable3)]
+    let maxS = [max1,max2,max3]
+    for (let index = 0; index < Counters.length; index++) {
+        const element = Counters[index];
+        if (element == 1){
+            for (let i = 0; i < maxS.length; i++) {
+                const SelectMax = maxS[i];
+                if (i!=index){
+                    if (SelectMax.indexOf(maxS[index][0]) !=-1 ){
+                        SelectMax.splice(SelectMax.indexOf(maxS[index][0]),1)     
+                    }
+                }
+            }
+        }
+    }
+    clearTimeout(timeout1)
+    clearTimeout(timeout2)
+    clearTimeout(timeout3)
+    console.log(`${max1}!!${max2}!!${max3}`)
+    console.log(`${max1.length}!!${max2.length}!!${max3.length}`)
+    var {indice , MaxSort} = sort(max1,max2,max3)  
+    if (MaxSort[0].length <=3){
+        var indexprior = -1
+        for (let index = 0; index < MaxSort[0].length; index++) {
+            const element = MaxSort[0][index];
+            if (max1.indexOf(element) != -1 && max2.indexOf(element) != -1 && max3.indexOf(element) != -1 ){
+                indexprior = index
+            }
+        }
+        if (indexprior != -1){
+            choose1 = MaxSort[0][indexprior]
+            MaxSort[0].splice(indexprior,1)
+        } else {
+            choose1 = generateFirstElement(MaxSort)
+        }
+    } else {
+        choose1 = generateFirstElement(MaxSort)
+    }
+    choose2 = generateSecondElement(MaxSort,choose1)
+    choose3 = generateThirdElement(MaxSort,choose1,choose2)
+
+    tempList = [choose1,choose2,choose3]
+
+    let OrderChoose = [-1,-1,-1]
+    for (let index = 0; index < OrderChoose.length; index++) {
+        OrderChoose[indice[index]] = tempList[index]
+    }
+    timeout1 = setTimeout(changeimage.bind(null,team1,`images/${NameNumber[OrderChoose[0]]}.png`),1000)
+    timeout2 = setTimeout(changeimage.bind(null,team2,`images/${NameNumber[OrderChoose[1]]}.png`),3000)
+    timeout3 = setTimeout(changeimage.bind(null,team3,`images/${NameNumber[OrderChoose[2]]}.png`),6000)
+    
 }
-/* TEST
-for (let index = 0; index < 10000; index++) {
-    generate()
-    console.log(`run:${index}`)
-}
-console.log("PASS")
+
+/* Good print
+    console.log(`${max1}!!${max2}!!${max3}`)
+    console.log(`${max1.length}!!${max2.length}!!${max3.length}`)
 */
+
+//TEST
+function test(){
+    for (let index = 0; index < 10000; index++) {
+        generate2()
+        console.log(`run:${index}`)
+    }
+    console.log("PASS")
+    return -1
+}
